63bedcc8245163f8fb666b3d22fc4d73569992e9
domingo1021 <domingo0204@gmail.com>
2022-05-04 16:37:04 +0800

    5/4 learn different js loops

---
+++ Loop/test.js
@@ -0,0 +1,59 @@
+// For loop
+console.log("Normal for loop")
+for(let i=0; i<5; i++){
+    console.log(i)  // expect to print i=0~4;
+}
+
+console.log("-------for each -------------");
+// normal function
+array_ = [1, 2, 3, 4, 5, 6, 7, 8];
+array_.forEach(function(element,index) {
+    element += 1;
+    array_[index] = element;
+});
+// arrow function
+array_.forEach((element,index)=>{
+    element+= 100;
+    array_[index] = element;
+})
+console.log(array_)
+
+
+console.log("----------do while--------")
+
+// do while loop, 
+// which will do the operation at first, 
+// then check for condition in while();
+let index = 0;
+do{
+    console.log(`Hello ${index} !`);
+    index +=1;
+}while(index<0);
+
+console.log("-----------while------------")
+
+// while loop
+let condition = 0;
+while (condition <5){
+    console.log(`number of condition: ${condition}`);
+    condition+=1;
+}
+
+console.log("----------for .. in ...------------")
+
+// for(.. in object)
+let student = {
+    id: 12345,
+    name: "John",
+    favorite_color: ["yellow","red","blue"],
+}
+for(let key in student){
+    console.log(`Key-value in student object --> ${key}: ${student[key]}`);
+}
+
+console.log("----------for .. of ...------------")
+
+// only for iterable key-value 
+for(let element of student.favorite_color){
+    console.log(`Element in iterable variable: ${element} `)
+}
--- Object_function/test.js
+++ Object_function/test.js
@@ -1,49 +1,64 @@
-let teacher = {
-    "name":"Domingo",
-    "students":["a","b","c","d",],
-    // Original definition of function
-    sayHi_0: function(){
-        console.log(`Hello world 0`)
-    },
-    // Simplified function
-    sayHi_1(){
-        console.log(`Hello world_1~ My name is ${this.name}`);
-    },
-    // Arrow function, added since ES6.
-    sayHi_2: () =>{
-      console.log(`hello world_2`);  
-    },
-    studentsHI(){
-        let msg="";
-        this.students.forEach(function(student){
-            msg += `Teacher ${this.name} say hello to ${student}, Good morning!\n`;
-        }.bind(this))
-        console.log(msg);
-    },
+let car = new Object();
+car.shape = "Big car"
+console.log(typeof car);
+console.log(car)
+
+let id = Symbol("id");
+
+let car_2 = {
+    shape : "Big car",
+    shape: "Bigggggggg",
+    "hello" : "world",
+    id: 12345,
 }
+console.log(car_2["id"]);
 
-teacher.sayHi_0();
-teacher.sayHi_1()
-teacher.sayHi_2()
-teacher.studentsHI();
+// let teacher = {
+//     "name":"Domingo",
+//     "students":["a","b","c","d",],
+//     // Original definition of function
+//     sayHi_0: function(){
+//         console.log(`Hello world 0`)
+//     },
+//     // Simplified function
+//     sayHi_1(){
+//         console.log(`Hello world_1~ My name is ${this.name}`);
+//     },
+//     // Arrow function, added since ES6.
+//     sayHi_2: () =>{
+//       console.log(`hello world_2`);  
+//     },
+//     studentsHI(){
+//         let msg="";
+//         this.students.forEach(function(student){
+//             msg += `Teacher ${this.name} say hello to ${student}, Good morning!\n`;
+//         }.bind(this))
+//         console.log(msg);
+//     },
+// }
 
-console.log("------To print out all key and value-------")
-console.log("First answer");
-let classmates=[
-    {"id":1, "name":"AAA"},
-    {"id":2, "name":"BBB"},
-    {"id":3, "name":"CCC"},
-]
-// First Answer
-classmates.forEach(function(classmate){
-    for(let key of Object.keys(classmate)){
-        console.log(`${key}: ${classmate[key]}`);
-    }
-})
-console.log("Second answer");
-// Second Answer (faster)
-classmates.forEach(function(classmate){
-    for(let [key, value] of Object.entries(classmate)){
-        console.log(`${key}: ${value}`);
-    }
-})
+// teacher.sayHi_0();
+// teacher.sayHi_1()
+// teacher.sayHi_2()
+// teacher.studentsHI();
+
+// console.log("------To print out all key and value-------")
+// console.log("First answer");
+// let classmates=[
+//     {"id":1, "name":"AAA"},
+//     {"id":2, "name":"BBB"},
+//     {"id":3, "name":"CCC"},
+// ]
+// // First Answer
+// classmates.forEach(function(classmate){
+//     for(let key of Object.keys(classmate)){
+//         console.log(`${key}: ${classmate[key]}`);
+//     }
+// })
+// console.log("Second answer");
+// // Second Answer (faster)
+// classmates.forEach(function(classmate){
+//     for(let [key, value] of Object.entries(classmate)){
+//         console.log(`${key}: ${value}`);
+//     }
+// })
